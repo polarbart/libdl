126fb51c11fbd0ad7f853fee692c56f12e3af850
polarbabe <jhansjakob@googlemail.com>
2019-05-02 23:29:57 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
-//
-// Created by polarbabe on 01.05.19.
-//
+#include <iostream>
+#include <Eigen/Dense>
+#include <spdlog/spdlog.h>
+#include <catch2/catch.hpp>
+
+using Eigen::MatrixXd;
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+
+int main() {
+    MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+
+    spdlog::info("Welcome to spdlog!");
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
 
